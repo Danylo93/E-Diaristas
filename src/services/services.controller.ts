1e4a4d87a73a9b243674705a8129c6e7ad1a76aa
Danylo93 <danylo.oliveira73@gmail.com>
2022-11-19 13:08:10 -0300

    Finalizando CRUD

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,20 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Render, Redirect, Request, UseFilters } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Render, Redirect, Request, UseFilters } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
-import { Repository } from 'typeorm';
-import { Service } from './entities/service.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { CreateException } from 'src/common/filters/create-exceptions.filter';
-import { Utils } from 'src/utils/utils';
+import { PatchException } from 'src/common/filters/patch-exceptions.filter';
 
 @Controller('admin/services')
 export class ServicesController {
   constructor(
       private readonly servicesService: ServicesService,
-      private readonly utils: Utils,
-      @InjectRepository(Service)
-     private readonly servicosRepository: Repository<Service>) {}
+      ) {}
 
   @Get('create')
   @Render('services/cadastrar')
@@ -25,75 +20,34 @@ export class ServicesController {
   @Get('index')
   @Render('services/index')
  async listarServicos() {
-    const servicos = await this.servicosRepository.find();
-    return {servicos: servicos};
+    
+    return {servicos: await this.servicesService.findAll()};
   }
 
   @Post()
   @UseFilters(CreateException)
   @Redirect('/admin/services/index')
  async cadastrar(@Body() createServiceDto: CreateServiceDto) {
-  createServiceDto.valorBanheiro = this.utils.formatDecimal(
-    createServiceDto.valorBanheiro,
-  );
-  createServiceDto.valorCozinha = this.utils.formatDecimal(
-    createServiceDto.valorCozinha,
-  );
-  createServiceDto.valorMinimo = this.utils.formatDecimal(
-    createServiceDto.valorMinimo,
-  );
-  createServiceDto.valorQuintal = this.utils.formatDecimal(
-    createServiceDto.valorQuintal,
-  );
-  createServiceDto.valorSala = this.utils.formatDecimal(
-    createServiceDto.valorSala,
-  );
-  createServiceDto.valorOutros = this.utils.formatDecimal(
-    createServiceDto.valorOutros,
-  );
-  createServiceDto.valorquarto = this.utils.formatDecimal(
-    createServiceDto.valorquarto,
-  );
-   return await this.servicosRepository.save(createServiceDto);
+  
+   return await this.servicesService.create(createServiceDto);
   }
 
   @Get(':id/edit')
   @Render('services/editar')
- async atualizarServico(@Param('id') id: number) {
-    const servico = await this.servicosRepository.findOneBy({id: id});
-    return {servico: servico};
+ async atualizarServico(@Param('id') id: number, @Request() req) {
+    return{message: req.flash('message'), oldData: req.flash('oldData'), alert: req.flash('alert'),
+    servico: await this.servicesService.findOne(id),
+  };
   }
 
   @Patch(':id/edit')
+  @UseFilters(PatchException)
   @Redirect('/admin/services/index')
- async update(@Param('id') id: string, @Body() updateServiceDto: UpdateServiceDto) {
-  updateServiceDto.valorBanheiro = this.utils.formatDecimal(
-    updateServiceDto.valorBanheiro,
-  );
-  updateServiceDto.valorCozinha = this.utils.formatDecimal(
-    updateServiceDto.valorCozinha,
-  );
-  updateServiceDto.valorMinimo = this.utils.formatDecimal(
-    updateServiceDto.valorMinimo,
-  );
-  updateServiceDto.valorQuintal = this.utils.formatDecimal(
-    updateServiceDto.valorQuintal,
-  );
-  updateServiceDto.valorSala = this.utils.formatDecimal(
-    updateServiceDto.valorSala,
-  );
-  updateServiceDto.valorOutros = this.utils.formatDecimal(
-    updateServiceDto.valorOutros,
-  );
-  updateServiceDto.valorquarto = this.utils.formatDecimal(
-    updateServiceDto.valorquarto,
-  );
-  return await this.servicosRepository.update(id, updateServiceDto);
-   
+ async update(
+  @Param('id') id: number, 
+  @Body() updateServiceDto: UpdateServiceDto,
+  ) {
+  return await this.servicesService.update(id, updateServiceDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.servicesService.remove(+id);
-  }
 }
